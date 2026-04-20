6c16aeed886ca9e053ee02b6d7b44890153b4f4f
Mukund-Menon <mukundmenon12005@gmail.com>
2026-04-20 22:52:20 +0530

    feat: add match feature function

---
+++ modules/matcher.py
@@ -0,0 +1,21 @@
+import cv2
+
+def match_features(desc1, desc2, ratio_thresh=0.75):
+
+    if desc1 is None or desc2 is None:
+        return []
+
+    if len(desc1) == 0 or len(desc2) == 0:
+        return []
+
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+
+    knn_matches = bf.knnMatch(desc1, desc2, k=2)
+
+    good_matches = []
+
+    for m, n in knn_matches:
+        if m.distance < ratio_thresh * n.distance:
+            good_matches.append(m)
+
+    return good_matches
